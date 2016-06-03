f5e00b7137a42b04e57062eb8f4e76ec4f966d3c
Thom <contacttomquinn@gmail.com>
2016-06-03 17:24:37 -0400

    Add GameSeries method to filter by number of players

--- PyertoRico/PyertoRico.py
+++ PyertoRico/PyertoRico.py
@@ -6,21 +6,27 @@ import webbrowser
 import requests
 import re
 
+###########################################################
+### Define generic classes for Board Game Arena data
+
 class GameSeries:
     
+    # Initialize a list of Game objects from a list of table IDs
     def __init__(self, tableIDs):
         games = []
         for tableID in tableIDs:
             games.append(Game(tableID))
         self.games = games
         
-class PRSeries(GameSeries):
-    
-    def __init__(self, tableIDs):
-        games = []
-        for tableID in tableIDs:
-            games.append(PuertoRico(tableID))
-        self.games = games
+    # Filter GameSeries to include only games with n players
+    def playerFilter(self, n):
+        games_subset = []
+        for game in self.games:
+            if(len(set(game.turnorder)) is n):
+                games_subset.append(game)
+        series_new = self
+        series_new.games = games_subset
+        return(series_new)
         
 class Game:
     
@@ -66,6 +72,73 @@ class Game:
         
         # Return a list of Role objects
         return(roles)
+        
+class Role:
+    
+    # Initialize a Role object by parsing a "role block"
+    def __init__(self, roleblock):
+      self.roleblock = roleblock
+      role_summary = Role.parse(roleblock)
+      self.player_name = role_summary[0]["{player_name}"]
+      self.rol_type = role_summary[1]["{rol_type}"]
+      self.role = role_summary[2::]
+    
+    # Parse each "role block" into a list of move logs
+    def parse(roleblock):
+        
+        # Find the first 'val' that corresponds to the supplied "{key}"
+        def key2val(key, rolechunk):
+            key_search = "\"" + key[1:-1] + "\"" + ":"
+            key_loc = rolechunk.find(key_search)
+            if rolechunk[key_loc + len(key_search)] == "\"":
+                start = key_loc + len(key_search) + 1
+                end = rolechunk.find("\"", start)
+            else:
+                start = key_loc + len(key_search)
+                end1 = rolechunk.find(",", start)
+                end2 = rolechunk.find("}", start)
+                end = min(end1, end2)
+            return({key: rolechunk[start:end]})
+        
+        # Retrieve important role information
+        keys = ("{player_name}", "{rol_type}")
+        role_summary = [key2val(key, roleblock) for key in keys] 
+        
+        # Retrieve the role logs
+        logs = []
+        log_loc = 0
+        while(log_loc > -1):
+            
+            # Locate each role log
+            log_loc = roleblock.find("\"log\":", log_loc + 1)
+            if log_loc == -1:
+                continue
+            log_val = key2val("\"log\":", roleblock[log_loc:])
+            log_val = [str(val) for (i, val) in log_val.items()][0]
+            if log_val == "${player_name} selected the ${rol_type_tr}":
+                continue
+            
+            # Populate missing "${key}" details in role log
+            if len(log_val) > 0:
+                args = re.findall("{\w*}", log_val)
+                vals = [key2val(arg, roleblock[log_loc:]) for arg in args]
+                for d in vals:
+                    log_val = [log_val.replace(k,v) for (k,v) in d.items()][0]
+                logs.append(log_val)
+        
+        # Return the role summary and role logs
+        return(role_summary + logs)
+        
+###########################################################
+### Define specific sub-classes for Puerto Rico game data
+
+class PRSeries(GameSeries):
+    
+    def __init__(self, tableIDs):
+        games = []
+        for tableID in tableIDs:
+            games.append(PuertoRico(tableID))
+        self.games = games
         
 # Define PuertoRico as a sub-class of Game
 # While the Game object includes methods general to (all of?) BGA,
@@ -234,59 +307,3 @@ class PuertoRico(Game):
             return(best_plyr[0])
         else:
             return(None)
-            
-class Role:
-    
-    # Initialize a Role object by parsing a "role block"
-    def __init__(self, roleblock):
-      self.roleblock = roleblock
-      role_summary = Role.parse(roleblock)
-      self.player_name = role_summary[0]["{player_name}"]
-      self.rol_type = role_summary[1]["{rol_type}"]
-      self.role = role_summary[2::]
-    
-    # Parse each "role block" into a list of move logs
-    def parse(roleblock):
-        
-        # Find the first 'val' that corresponds to the supplied "{key}"
-        def key2val(key, rolechunk):
-            key_search = "\"" + key[1:-1] + "\"" + ":"
-            key_loc = rolechunk.find(key_search)
-            if rolechunk[key_loc + len(key_search)] == "\"":
-                start = key_loc + len(key_search) + 1
-                end = rolechunk.find("\"", start)
-            else:
-                start = key_loc + len(key_search)
-                end1 = rolechunk.find(",", start)
-                end2 = rolechunk.find("}", start)
-                end = min(end1, end2)
-            return({key: rolechunk[start:end]})
-        
-        # Retrieve important role information
-        keys = ("{player_name}", "{rol_type}")
-        role_summary = [key2val(key, roleblock) for key in keys] 
-        
-        # Retrieve the role logs
-        logs = []
-        log_loc = 0
-        while(log_loc > -1):
-            
-            # Locate each role log
-            log_loc = roleblock.find("\"log\":", log_loc + 1)
-            if log_loc == -1:
-                continue
-            log_val = key2val("\"log\":", roleblock[log_loc:])
-            log_val = [str(val) for (i, val) in log_val.items()][0]
-            if log_val == "${player_name} selected the ${rol_type_tr}":
-                continue
-            
-            # Populate missing "${key}" details in role log
-            if len(log_val) > 0:
-                args = re.findall("{\w*}", log_val)
-                vals = [key2val(arg, roleblock[log_loc:]) for arg in args]
-                for d in vals:
-                    log_val = [log_val.replace(k,v) for (k,v) in d.items()][0]
-                logs.append(log_val)
-        
-        # Return the role summary and role logs
-        return(role_summary + logs)
